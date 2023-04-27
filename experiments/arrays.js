ebbcb97f20f3357de851445fb87e6af48b55bf95
Quinn-UCSC <migquinn@ucsc.edu>
2023-04-27 12:36:01 -0700

    Lecture Example (4/27/2023)
    -Will edit later into an experiment tab

---
+++ experiments/arrays.js
@@ -0,0 +1,36 @@
+/*
+*   Author: Michael Quinn
+*   Date: 04-27-2023
+*   Description: Practing Arrays
+*/
+
+//Constants
+var array = [1,2,3,4];
+var shopping_list = ["yellow onions", "bananas", "milk", "eggs", "H202", "chips", "freedom bread", "apples", "Yugioh 1 pack"];
+var x = 1;
+var last = shopping_list.pop();    //Removes and returns last element
+var first = shopping_list.shift();    //Removes and returns last element
+var sort = shopping_list.sort();    //Sorts the array
+var reverse = shopping_list.reverse();    //Reverses the order of the array
+var newArray = ["zeroth element", 
+["one", "two", 3],
+"last element"];
+var myRide = [];
+myRide.make = "Ford";
+myRide.model = "Taurus";
+myRide.color = "Grey";
+myRide.year = "1995";
+myRide.extras = ["torn seats", "leaky moon roofs", "broken cassette player"];
+var transportation = [myRide];
+
+//Functions
+function main() {
+    console.log(shopping_list[1]);
+    console.log(shopping_list.length);
+    shopping_list.push = "chorizo";     //Adds new element to the end of the array
+    console.log(shopping_list);
+    console.log(newArray[newArray.length - 1]);
+    console.log(newArray[1][2]);
+    console.log("My ride is a", myRide.color, myRide.year, myRide.make, myRide.model, myRide.extras);
+    document.writeln("My ride is a", myRide.color, myRide.year, myRide.make, myRide.model, myRide.extras);
+}
