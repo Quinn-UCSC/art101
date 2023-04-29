644dd5af6f6494d770c0580294af613dded85d28
Quinn-UCSC <migquinn@ucsc.edu>
2023-04-29 14:00:31 -0700

    Created Lab 6 page with lab partner
    -Worked on lab 6 page
    -Updated tabs

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Is to practice with arrays and functions.
+// Author: Michael Quinn and Kira
+// Date: 4/29/2023
 
 // Constants
+var myTransport = ["Car", "Bus", "Plane", "Boat", "Spaceship", "Submarine"];
+var myMainRide = {
+  make: "",
+  model: "",
+  color: "",
+  year: "",
+    age: function() {
+    return 2023 - this.year;
+  }
+} 
+
+myMainRide.make = "Ford";
+myMainRide.model = "Taurus";
+myMainRide.color = "Blue";
+myMainRide.year = "2040";
 
 // Functions
+  
+//For loop for myTransport
+  for (let i = 0; i < myTransport.length; i++) {
+    document.writeln("This is my transport: " + myTransport[i] + ".<br>");
+  }
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+  // Printing out object of myMainRide
+  document.writeln("My Main Ride is: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // let's get this party started
 main();
