fbea530865b0c78256ea7e5171be4fac41a31a7e
Quinn-UCSC <migquinn@ucsc.edu>
2023-05-10 23:43:55 -0700

    Updated lab2 header
    -Added "shaky-text" class to make text shake.
    -Failed experimental text commented out in js.

--- lab2/js/lab.js
+++ lab2/js/lab.js
@@ -5,17 +5,40 @@
 // Constants
 
 // Functions
+// var oldEl = document.getElementsByClassName("shakytext");
+// var myString = "Hello World";
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// var draw_x = 32;
+// var draw_y = 32;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// var intensity = 2; //shakeyness
 
-// let's get this party started
-main();
+// //get how long the string is in terms of width
+// var length = string_length(string(myString));
+
+// for(var i=1;i<=length;i++) { //loop through each character of the string
+//     var str = string_copy(myString,i,1); //1 character of the string 
+//     var length_so_far = string_width(string_copy(myString,1,i)); //how far to offset the text
+//     draw_text(draw_x+length_so_far+random_range(-intensity,intensity),draw_y+random_range(-intensity,intensity),str);
+// }
+
+// const textElement = document.getElementById('ut-text');
+// const text = textElement.textContent;
+// textElement.textContent = '';
+
+// var draw_x = 32;
+// var draw_y = 32;
+
+// var intensity = 2; //shakeyness
+// for (const char of text) {
+//   const span = document.createElement('span');
+//    for(var i=1;i<=length;i++) { //loop through each character of the string
+//      var str = string_copy(span,i,1); //1 character of the string 
+//      var length_so_far = string_width(string_copy(span,1,i)); //how far to offset the text
+//      draw_text(draw_x+length_so_far+random_range(-intensity,intensity),draw_y+random_range(-intensity,intensity),str);
+// }
+// span.textContent = char;
+// textElement.appendChild(span);
+// }
+
+// oldEl.appendChild(myString);
