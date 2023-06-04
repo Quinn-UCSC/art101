aafe14cb2c04cc54cadea1d62aea2580f99cf69e
Quinn-UCSC <migquinn@ucsc.edu>
2023-06-04 04:03:20 -0700

    Updated pokedex page.
    -Possibly finished. Really satisfied with the results.

--- experiments/pokedex.js
+++ experiments/pokedex.js
@@ -1,11 +1,74 @@
 $(document).ready(function() {
     var pokedexElement = $("#pokemon-container");
     var randomizeButton = $("#randomize-button");
+    var powerButton = $(".power-button");
     var screenElement = $(".screen");
+    var isPokedexOn = true;
+    var audio = new Audio(); // Create an audio object without specifying the source initially
+    var volumeStep = 0.1; // Volume step value
+    audio.volume = 0.7;
+
+    powerButton.on("click", function() {
+      if (isPokedexOn) {
+        turnOffPokedex();
+      } else {
+        turnOnPokedex();
+      }
+    });
   
+    function turnOnPokedex() {
+      // Display loading GIF
+      var loadingImage = $("<img>").attr("src", "https://thumbs.gfycat.com/AcclaimedUnsightlyAngelfish-size_restricted.gif").addClass("loading-gif");
+      screenElement.append(loadingImage);
+      playSoundEffect("../audio/01_Opening_Movie.mp3");
+  
+      // Simulate delay before showing the Pokedex
+      setTimeout(function() {
+        loadingImage.remove();
+        playSoundEffect("../audio/startup.mp3");
+        isPokedexOn = true;
+        powerButton.addClass("on");
+        getRandomPokemon();
+      }, 10000);
+    }
+  
+    function turnOffPokedex() {
+      isPokedexOn = false;
+      powerButton.removeClass("on");
+      pokedexElement.empty();
+    }
+  
+    function getRandomNumber() {
+      return Math.ceil(Math.random() * 37);
+    }
+
     randomizeButton.on("click", function() {
+      playSoundEffect("../audio/SFX_PRESS_AB.wav");
+      var cry = "SFX_CRY_" + getRandomNumber() + ".wav";
+      playSoundEffect(cry);
       getRandomPokemon();
     });
+
+    function playSoundEffect(soundFile) {
+      // audio.pause(); // Pause the currently playing audio, if any
+      audio.src = "../audio/" + soundFile; // Set the new audio source
+      audio.play(); // Play the audio
+    }
+
+    $("#controller_up").on("click", function() {
+      showVolumeAdjustmentText("Volume: " + audio.volume)
+      adjustVolume(volumeStep);
+    });
+  
+    $("#controller_down").on("click", function() {
+      showVolumeAdjustmentText("Volume: " + audio.volume)
+      adjustVolume(-volumeStep);
+    });
+  
+    function adjustVolume(step) {
+      audio.volume += step;
+      audio.volume = Math.max(0, Math.min(1, audio.volume)); // Ensure volume stays within the range [0, 1]
+    }
   
     function getRandomPokemon() {
       var randomId = Math.floor(Math.random() * 898) + 1; // Generate a random Pokémon ID between 1 and 898
@@ -52,7 +115,7 @@ $(document).ready(function() {
 
           // Continue making Pokémon card
           var pokemonImage = $("<img>").addClass("pokemon-image").attr("src", pokemonData.sprites.front_default);
-          var pokemonAttributes = $("<ul>");
+          var pokemonAttributes = $("<ul>").addClass("basestats");
           
           // Add each attribute/statistic to the Pokémon card
           for (var i = 0; i < pokemonData.stats.length; i++) {
@@ -71,8 +134,8 @@ $(document).ready(function() {
           pokemonCard.append(pokemonImage);
           pokemonCard.append(pokemonAttributesContainer);
           pokemonCard.append(pokemonImage);
+          pokemonCard.append(pokemonAttributes);
           pokedexElement.append(pokemonCard);
-          pokedexElement.append(pokemonAttributes);
         },
         error: function(jqXHR, textStatus, errorThrown) {
           console.log("Error:", textStatus, errorThrown);
@@ -101,6 +164,21 @@ $(document).ready(function() {
 
   //   return spacing + "px";
   // }
+
+  // Show volume adjustment text and fade it out after a delay
+  function showVolumeAdjustmentText(text) {
+    var volumeText = $("<h2>")
+      .addClass("volume-text")
+      .text(text)
+      .css({
+        top: "20px",
+        left: "20px"
+      })
+      .appendTo("#begin")
+      .fadeOut(2000, function() {
+        $(this).remove();
+      });
+  }
 
     // Initial random Pokémon
     getRandomPokemon();
