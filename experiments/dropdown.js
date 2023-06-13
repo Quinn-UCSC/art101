f3721969dbfb5e7f9d092ec5401c02c7f92fa3ff
Quinn-UCSC <migquinn@ucsc.edu>
2023-06-12 22:50:08 -0700

    Updated dropdown.js

--- experiments/dropdown.js
+++ experiments/dropdown.js
@@ -1,14 +1,31 @@
-// JavaScript to handle the dropdown functionality
 window.addEventListener('DOMContentLoaded', function() {
-    var dropdownItems = document.querySelectorAll('.dropdown-menu');
-  
-    dropdownItems.forEach(function(item) {
-      item.parentNode.addEventListener('mouseenter', function() {
-        item.style.display = 'block';
-      });
-  
-      item.parentNode.addEventListener('mouseleave', function() {
+  var dropdownItems = document.querySelectorAll('.dropdown-menu');
+
+  dropdownItems.forEach(function(item) {
+    item.parentNode.addEventListener('mouseenter', function() {
+      item.style.display = 'block';
+    });
+
+    item.parentNode.addEventListener('mouseleave', function() {
+      // Check if the mouse is still hovering over the dropdown menu
+      var isHovered = isMouseHovering(item);
+
+      if (!isHovered) {
         item.style.display = 'none';
-      });
+      }
     });
   });
+
+  function isMouseHovering(element) {
+    var rect = element.getBoundingClientRect();
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+
+    return (
+      mouseX >= rect.left &&
+      mouseX <= rect.right &&
+      mouseY >= rect.top &&
+      mouseY <= rect.bottom
+    );
+  }
+});
